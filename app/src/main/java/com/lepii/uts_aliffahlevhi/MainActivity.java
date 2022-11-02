a0676c30f7dfc3ef23b49647d20511659300229b
FhLevi13 <alifvahlephi46@gmail.com>
2022-11-02 21:07:05 +0700

    02/11/2022

--- app/src/main/java/com/lepii/uts_aliffahlevhi/MainActivity.java
+++ app/src/main/java/com/lepii/uts_aliffahlevhi/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -15,42 +17,58 @@ public class MainActivity extends AppCompatActivity {
     Button btnDaftar;
     EditText etNama;
     EditText etNoPendaftaran;
-    Spinner spJalurPendaftaran;
     CheckBox cbKonfirmasi;
+    AutoCompleteTextView actvJalurPendaftaran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("Home");
+        getSupportActionBar().setTitle("Formulir Pendaftaran");
+
+        String[] type = new String[] {"Tes Tertulis", "Jalur Tanpa Tes", "Ujian Saingan Masuk"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.drop_down_item,
+                type
+        );
 
         etNama = findViewById(R.id.et_namalengkap);
         etNoPendaftaran = findViewById(R.id.et_nomorpendaftaran);
-        spJalurPendaftaran = findViewById(R.id.sp_jalurpendaftaran);
+        actvJalurPendaftaran = findViewById(R.id.atv_jalur_pendaftaran);
+        actvJalurPendaftaran.setAdapter(adapter);
         cbKonfirmasi = findViewById(R.id.cb_konfirmasi);
         btnDaftar = findViewById(R.id.btn_daftar);
+
 
         btnDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String Nama = etNama.getText().toString();
                 String NoPendaftaran = etNoPendaftaran.getText().toString();
-
+                String JalurPendaftaran = actvJalurPendaftaran.getText().toString();
 
                 if (Nama.trim().equals("")) {
                     etNama.setError("Nama Tidak Boleh Kosong");
                 }
-                if else (NoPendaftaran.trim().equals("")) {
-                    etNoPendaftaran.setError("Nama Tidak Boleh Kosong");
+                else if (NoPendaftaran.trim().equals("")) {
+                    etNoPendaftaran.setError("No.Pendaftaran Tidak Boleh Kosong");
+                }
+                else if (JalurPendaftaran.trim().isEmpty()) {
+                    actvJalurPendaftaran.setError("Pilih Jalur Pendaftaran Terlebih Dahulu");
                 }
                 else {
-                    Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
-                    pindah.putExtra("inputNama", Nama);
-                    pindah.putExtra("inputNoPendaftaran", NoPendaftaran);
-                    pindah.putExtra("inputJalurPendaftaran", Nama);
-                    pindah.putExtra("inputNoPendaftaran", NoPendaftaran);
-                    startActivity(pindah);
+                    if (cbKonfirmasi.isChecked()) {
+                        Intent daftar = new Intent(MainActivity.this, SecondActivity.class);
+                        daftar.putExtra("inputNama", Nama);
+                        daftar.putExtra("inputNoPendaftaran", NoPendaftaran);
+                        daftar.putExtra("inputJalurPendaftaran", JalurPendaftaran);
+                        startActivity(daftar);
+                    }
+                    else {
+                        cbKonfirmasi.setError("Konfirmasi Terlebih Dahulu");
+                    }
                 }
             }
         });
